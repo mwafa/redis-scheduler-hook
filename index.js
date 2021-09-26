39a0757da0af1cd0c623ed9a79c6491269b3018e
Muhammad Wafa <wafax.4@gmail.com>
2021-09-27 04:06:43 +0700

    add interval env

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require("dotenv").config()
 const { listenSchedule } = require("./src/scheduler")
 const server = require("./src/server")
+const logger = require("./src/utils/logger")
 
 const port = process.env.PORT || 3000
+const interval = process.env.INTERVAL || 3000
 
 server.listen(port, () => {
-  console.log(`Server run on port ${port}`)
-  listenSchedule()
+  logger(`Server run on port ${port}`)
+  listenSchedule(interval)
 })
